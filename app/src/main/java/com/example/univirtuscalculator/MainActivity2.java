690843abc8f0890bb13a7899fce6ff09b460ce7a
Poveda <julianopoveda@github.com>
2020-11-02 00:53:01 -0300

    Change invoke math operations to switch
    The invoke approach was raised nullpointerexception, even though the
    class is knowing by the activity

--- app/src/main/java/com/example/univirtuscalculator/MainActivity2.java
+++ app/src/main/java/com/example/univirtuscalculator/MainActivity2.java
@@ -2,74 +2,55 @@ package com.example.univirtuscalculator;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import static java.lang.Float.parseFloat;
 
-public class MainActivity2 extends AppCompatActivity{
+public class MainActivity2 extends AppCompatActivity {
 
     TextView display;
     String displayNumber;
     Float operand = null;
     Float operator = null;
-    private Method operationMethod;
+    private String operationMethod;
     private MathOperations mathOperations;
+    private String currentOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         display = findViewById(R.id.textView);
         displayNumber = new String();
         mathOperations = new MathOperations();
+        currentOperation = new String();
 
-        //Button number0 = (Button) findViewById(R.id.btnNumber0);
-        //number0.setOnClickListener(numberListener);
-
+        // Region listener binding
         Button clear = (Button) findViewById(R.id.btnClear);
         clear.setOnClickListener(clearListener);
 
         Button dot = (Button) findViewById(R.id.btnDot);
-        dot.setOnClickListener(dotListener);
+        dot.setOnClickListener(decimalSimbolListener);
 
         Button equal = (Button) findViewById(R.id.btnEquals);
         equal.setOnClickListener(operationResultListener);
 
+        //for the number and operation buttons I dinamicly get every button and apply the correct bind
         for (Field field : R.id.class.getFields()) {
             try {
                 int fieldId = field.getInt(0);
-                if(field.getName().startsWith("btnNumber"))
-                {
+                if (field.getName().startsWith("btnNumber")) {
                     Button number = null;
                     number = (Button) findViewById(fieldId);
                     number.setOnClickListener(numberListener);
-                }
-                else if(field.getName().startsWith("btnOperation"))
-                {
+                } else if (field.getName().startsWith("btnOperation")) {
                     Button operation = null;
                     operation = (Button) findViewById(fieldId);
                     operation.setOnClickListener(operationListener);
@@ -78,11 +59,13 @@ public class MainActivity2 extends AppCompatActivity{
                 e.printStackTrace();
             }
         }
+        //end region
     }
 
-    View.OnClickListener numberListener = new View.OnClickListener(){
+    // region listeners logic
+    View.OnClickListener numberListener = new View.OnClickListener() {
         @Override
-        public void onClick(View view){
+        public void onClick(View view) {
             Button pressedBtn = (Button) view;
             String number = pressedBtn.getText().toString();
             displayNumber += number;
@@ -90,77 +73,85 @@ public class MainActivity2 extends AppCompatActivity{
         }
     };
 
-    View.OnClickListener dotListener = new View.OnClickListener(){
+    View.OnClickListener decimalSimbolListener = new View.OnClickListener() {
         @Override
-        public void onClick(View view){
+        //Only print a decimal separator once per number
+        public void onClick(View view) {
             Button pressedBtn = (Button) view;
             String dot = pressedBtn.getText().toString();
-            if(displayNumber != "" && displayNumber.indexOf(".") == -1)
+            if (displayNumber != "" && displayNumber.indexOf(".") == -1 && !displayNumber.endsWith("."))
                 displayNumber += dot;
+            if(displayNumber.contains(currentOperation+"."))
+            {
+                displayNumber = displayNumber.substring(0, displayNumber.length() - 2) + "0.";
+            }
             display.setText(displayNumber);
         }
     };
 
-    View.OnClickListener operationListener = new View.OnClickListener(){
+    View.OnClickListener operationListener = new View.OnClickListener() {
         @Override
-        public void onClick(View view){
+        public void onClick(View view) {
             Button pressedBtn = (Button) view;
             String operation = pressedBtn.getText().toString();
             String methodName = (String) pressedBtn.getTag();
-            try {
-                if(operand == null && displayNumber != "") {
-                    operand = parseFloat(displayNumber);
-                    displayNumber += " " + operation + " ";
-                    display.setText(displayNumber);
-                    //Set correct operation
-                    operationMethod = mathOperations.getClass().getDeclaredMethod(methodName, Float.TYPE, Float.TYPE);
-                }
-                else if(operand != null && operator == null){
-                    int operatorIndex = displayNumber.indexOf(operation);
-                    String operatorAsString = displayNumber.substring(operatorIndex+1);
 
-                    if(operatorAsString != "")
-                        operator = parseFloat(operatorAsString);
-                    else
-                        operator = operand;
+            //check wheter is a operand atribution or a operator atribution
+            if (operand == null && !displayNumber.equals("")) {
+                operand = parseFloat(displayNumber);
+                displayNumber += " " + operation + " ";
+                display.setText(displayNumber);
 
-                    operand = (float) operationMethod.invoke(null, operand, operator);
-                    operator = null;
-                    operationMethod = mathOperations.getClass().getMethod(methodName, Float.TYPE, Float.TYPE);
+                operationMethod = methodName;//set what operation may be performed
+                currentOperation = operation;//helper for cases when the present button differs of the current operation
+
+            } else if (operand != null && operator == null) {
+                int operatorIndex = displayNumber.indexOf(currentOperation);
+
+                //If the user press same operation 2 times, the calculator understand that the user wishes to perform the operation with the same number in the operand and operator
+                if (operatorIndex != -1 && !displayNumber.substring(operatorIndex + 1).equals(" ")) {
+                    String operatorAsString = displayNumber.substring(operatorIndex + 1);
+                    operator = parseFloat(operatorAsString);
                 }
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
+                else
+                    operator = operand;
+
+                operand = ResolveOperation();
+
+                displayNumber = String.valueOf(operand) + " " + operation + " ";
+                display.setText(displayNumber);
+                operator = null;
+                operationMethod = methodName;
             }
+
         }
     };
 
-    View.OnClickListener operationResultListener = new View.OnClickListener(){
+    View.OnClickListener operationResultListener = new View.OnClickListener() {
         @Override
-        public void onClick(View view){
-            try {
-                if (operand != null && operator != null) {
-                    float result = (float) operationMethod.invoke(null, operand, operator);
-                    displayNumber = String.valueOf(result);
-                    display.setText(displayNumber);
-                    operand = null;
-                    operator = null;
-                    operationMethod = null;
+        public void onClick(View view) {
+            if (operand != null) {
+                int operatorIndex = displayNumber.indexOf(currentOperation);
+
+                if (operatorIndex != -1 && !displayNumber.substring(operatorIndex + 1).equals(" ")) {
+                    String operatorAsString = displayNumber.substring(operatorIndex + 1);
+                    operator = parseFloat(operatorAsString);
                 }
-                else if (displayNumber != "") //If this heapens, only clean the variables, because the display already shows the result
-                {
-                    operand = null;
-                    operator = null;
-                    operationMethod = null;
-                    displayNumber = "";
-                }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
+                else
+                    operator = 0F;
+
+                float result = ResolveOperation();
+                displayNumber = String.valueOf(result);
+                display.setText(displayNumber);
+                operand = null;
+                operator = null;
+                operationMethod = null;
+            } else if (!displayNumber.equals("")) //If this heapens, only clean the variables, because the display already shows the result
+            {
+                operand = null;
+                operator = null;
+                operationMethod = null;
+                displayNumber = "";
             }
 
 
@@ -176,6 +167,21 @@ public class MainActivity2 extends AppCompatActivity{
             operator = null;
         }
     };
+    // endregion
 
-    
+    //Decides what operation should perform
+    public float ResolveOperation() {
+        switch (operationMethod) {
+            case "Plus":
+                return mathOperations.Plus(operand, operator);
+            case "Minus":
+                return mathOperations.Minus(operand, operator);
+            case "Multiplication":
+                return mathOperations.Multiplication(operand, operator);
+            case "Division":
+                return mathOperations.Division(operand, operator);
+            default:
+                return 0;
+        }
+    }
 }
